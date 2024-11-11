c87293cf0d723dec66be08754a1db3ec15c8cd61
Cristhian <cristhian.ramirez@icami.mx>
2024-11-11 16:34:40 -0600

    library

---
+++ app/Libraries/ENCRIPT.php
@@ -0,0 +1,34 @@
+<?php
+
+class ENCRIPT
+{
+	public static function decode($ciphertext, $key)
+	{
+            // Decodifica el texto encriptado
+        $ciphertext = base64_decode($ciphertext);
+        
+        // Extrae el IV y el ciphertext
+        $ivLength = openssl_cipher_iv_length('AES-256-CBC');
+        $iv = substr($ciphertext, 0, $ivLength);
+        $ciphertext = substr($ciphertext, $ivLength);
+        
+        // Desencripta el texto
+        return openssl_decrypt($ciphertext, 'AES-256-CBC', $key, 0, $iv);
+	}
+
+	public static function encode($plaintext)
+	{
+        //$key=ENV[];
+        // Genera un vector de inicialización (IV)
+        $ivLength = openssl_cipher_iv_length('AES-256-CBC');
+        $iv = openssl_random_pseudo_bytes($ivLength);
+        
+        // Encripta el texto
+        $ciphertext = openssl_encrypt($plaintext, 'AES-256-CBC', $key, 0, $iv);
+        
+        // Combina el IV y el ciphertext para poder desencriptarlo luego
+        return base64_encode($iv . $ciphertext);
+	}
+
+
+}
